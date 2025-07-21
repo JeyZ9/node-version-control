c84f1c16396d970b308c72bf4ed4a42ae9b70393
JeyZ9 <ggjj4511@gmail.com>
2025-07-21 18:57:55 +0700

    new api

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("hello node!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+})
+
 app.get(`/say-hello/:name`, (req, res) => {
   const name = req.params.name;
   res.send(`Hello ${name}`);
